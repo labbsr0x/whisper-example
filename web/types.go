c9b343c45f43fc3e7157babeaf630e65778cb87c
Claudio Segala Rodrigues Silva Filho <claudiosegalafilho@ibm.com>
2019-10-22 13:17:11 -0300

    Refactoring

---
+++ web/types.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/labbsr0x/whisper-client/client"
+
+// context holds the context of the application
+type context struct {
+	whisperClient *client.WhisperClient
+	whisperToken  string
+}
+
+// homePage holds the info necessary for the home page template
+type homePage struct {
+	LoginURL string
+}
+
+// dashboardPage holds the info necessary for the dashboard page template
+type dashboardPage struct {
+	Username string
+}
